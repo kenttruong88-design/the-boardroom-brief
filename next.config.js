e20e10fedc96fc47af6f2b4b9ca0d2a9fcb44794
Kent <kentt@theboardroombrief.local>
2026-04-04 11:48:55 +0200

    Fix next config syntax

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
 
-const nextConfig: NextConfig = {};
-
-export default nextConfig;
+module.exports = nextConfig
